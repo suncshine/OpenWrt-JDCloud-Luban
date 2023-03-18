97bc6233a85f2b89c4082b48daf23256097d0765
pmyy-wt <127957179+pmyy-wt@users.noreply.github.com>
2023-03-18 21:56:08 +0800

    Update diy-part1.sh
    fix error

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,9 +16,10 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git custom https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git luci https://github.com/openwrt/luci.git' >> feeds.conf.default
 echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+cat feeds.conf.default
