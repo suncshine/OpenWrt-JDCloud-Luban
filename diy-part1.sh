94280cd3cc501a67d73554d24e416a0894b9b9ef
pmyy-wt <127957179+pmyy-wt@users.noreply.github.com>
2024-03-12 15:32:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,11 +15,11 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git custom https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git luciex https://github.com/openwrt/luci.git' >> feeds.conf.default
-echo 'src-git packagesex https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git coolsnowwolf https://github.com/coolsnowwolf/packages' >>feeds.conf.default
 cat feeds.conf.default
